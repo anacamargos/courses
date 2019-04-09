9f40494595e605fb50040225755bb5e2d3ecdc29
anacamargos <alcvlara@sga.pucminas.br>
2019-04-09 16:02:29 -0300

    Adding alert

--- courses/Controller/CourseDetailViewController.swift
+++ courses/Controller/CourseDetailViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class CourseDetailViewController: UIViewController {
     
@@ -24,6 +26,25 @@ class CourseDetailViewController: UIViewController {
         descriptionTextView.text = currentCourse.description
         joinCourseButton.setTitle("Join \(currentCourse.instructor)", for: .normal)
         featuredImageView.image = currentCourse.featuredImage
+        
+        navigationItem.title = currentCourse.instructor
+    }
+    
+    @IBAction func playButtonDidTap () {
+        let videoURL = currentCourse!.introductionVideoURL
+        let player = AVPlayer(url: videoURL)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        self.present(playerViewController, animated: true, completion: {
+            playerViewController.player!.play()
+        })
+    }
+    
+    @IBAction func joinButtonDidTap () {
+        let alertController = UIAlertController(title: "Congratulations!", message: "You just enrolled in \(currentCourse.title) successfully!", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
     }
     
 }
